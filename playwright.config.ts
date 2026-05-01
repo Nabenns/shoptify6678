ecae3bdebd33b01e177efe25c99e603a92601758
Zay <nabhanggt@gmail.com>
2026-05-02 02:37:57 +0700

    feat: scaffold playwright project

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  workers: 3,
+  timeout: 120_000,
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['list'],
+  ],
+  projects: [
+    {
+      name: 'unit',
+      testMatch: '**/*.unit.spec.ts',
+    },
+    {
+      name: 'e2e',
+      testMatch: '**/trial-signup.spec.ts',
+      use: {
+        baseURL: process.env.BASE_URL,
+        headless: true,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+      },
+    },
+  ],
+});
